90d34f62d1781cbce379dde79f91cbe1d59d5bb4
QPCrummer <qpcrummer@gmail.com>
2025-07-12 01:43:17 -0400

    Add jitpack and begin work on test mod

---
+++ multimctest/src/main/java/com/github/tatercertified/multimctest/Multimctest.java
@@ -0,0 +1,10 @@
+package com.github.tatercertified.multimctest;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Multimctest implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
